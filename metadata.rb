7fbebf14f4634b937bf75827c964d44078122f1e
Roderik van der Veer <roderik@vanderveer.be>
2013-02-17 12:33:20 +0100

    Inital try

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'homebrew-apps'
+maintainer       'Roderik van der Veer'
+maintainer_email 'roderik@vanderveer.be'
+license          'MIT'
+description      'Installs and configures homebrew-apps'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
---
+++ recipes/bash4.rb
@@ -0,0 +1,3 @@
+package "bash" do
+  action :install
+end
