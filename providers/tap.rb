bfe6eca50e5cfc62dd10dd634130ae5ae22c4ed2
Roderik van der Veer <roderik@vanderveer.be>
2013-02-17 13:38:41 +0100

    Fix foodcritic errors

--- providers/tap.rb
+++ providers/tap.rb
@@ -1,27 +1,27 @@
 def load_current_resource
-  @tap = Chef::Resource::HomebrewTap.new(new_resource.name)
-  tap_dir = @tap.name.gsub('/', '-')
+    @tap = Chef::Resource::HomebrewTap.new(new_resource.name)
+    tap_dir = @tap.name.gsub('/', '-')
 
-  Chef::Log.debug("Checking whether we've already tapped #{new_resource.name}")
-  if ::File.directory?("/usr/local/Library/Taps/#{tap_dir}")
-    @tap.tapped true
-  else
-    @tap.tapped false
-  end
+    Chef::Log.debug("Checking whether we've already tapped #{new_resource.name}")
+    if ::File.directory?("/usr/local/Library/Taps/#{tap_dir}")
+      @tap.tapped true
+    else
+      @tap.tapped false
+    end
 end
 
 action :tap do
-  unless @tap.tapped
     execute "tapping #{new_resource.name}" do
-      command "/usr/local/bin/brew tap #{new_resource.name}"
+        command "/usr/local/bin/brew tap #{new_resource.name}"
+        not_if { @tap.tapped }
     end
-  end
+    new_resource.updated_by_last_action(true)
 end
 
 action :untap do
-  if @tap.tapped
     execute "untapping #{new_resource.name}" do
       command "/usr/local/bin/brew untap #{new_resource.name}"
+      only_if { @tap.tapped }
     end
-  end
+    new_resource.updated_by_last_action(true)
 end
