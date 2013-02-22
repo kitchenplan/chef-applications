2f1ec2c955fd4a1d602dab8a0c5ae8ea33393142
Roderik van der Veer <roderik@vanderveer.be>
2013-02-22 19:11:54 +0100

    Add more sublime packages

--- attributes/sublime_text.rb
+++ attributes/sublime_text.rb
@@ -6,5 +6,11 @@ node.default["sublime_text_packages"] = [
   {"name" => "SublimeCodeIntel", "source" => "http://github.com/Kronuz/SublimeCodeIntel"},
   {"name" => "Theme - Aqua", "source" => "https://github.com/cafarm/aqua-theme"},
   {"name" => "Tomorrow Color Schemes", "source" => "https://github.com/theymaybecoders/sublime-tomorrow-theme"},
-  {"name" => "DetectSyntax", "source" => "https://github.com/phillipkoebbe/DetectSyntax"}
+  {"name" => "DetectSyntax", "source" => "https://github.com/phillipkoebbe/DetectSyntax"},
+  {"name" => "SCSS", "source" => "https://github.com/kuroir/SCSS.tmbundle"},
+  {"name" => "PHPNamespaceCommand", "source" => "http://alexandre-salome.fr/blog/SublimeText-2-Insert-PHP-namespaces"},
+  {"name" => "SublimeLinter", "source" => "http://github.com/SublimeLinter/SublimeLinter"},
+  {"name" => "CoffeeScript", "source" => "http://xavura.github.com/CoffeeScript-Sublime-Plugin"},
+  {"name" => "Alignment", "source" => "http://wbond.net/sublime_packages/alignment" },
+  {"name" => "MarkdownPreview", "source" => "https://github.com/revolunet/sublimetext-markdown-preview"}
 ]
