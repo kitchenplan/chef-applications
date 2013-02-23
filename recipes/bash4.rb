2b41d9e7b32cd140b5305e0c696333f43738eed2
Roderik van der Veer <roderik@vanderveer.be>
2013-02-23 12:26:19 +0100

    set shells

--- recipes/bash4.rb
+++ recipes/bash4.rb
@@ -3,3 +3,15 @@ include_recipe "homebrew::default"
 package "bash" do
   action :install
 end
+
+execute "set the root user shell to bash" do
+  command "dscl . -create /Users/root UserShell /usr/local/bin/bash"
+end
+
+execute "set the root user shell to bash" do
+  command "dscl . -create /Users/#{node['current_user']} UserShell /usr/local/bin/bash"
+end
+
+link "/root" do
+    to "/var/root"
+end
