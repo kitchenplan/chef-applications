8d994eff78fd0adc7d12529c0ed21fc8e12683f7
Roderik van der Veer <roderik@vanderveer.be>
2013-02-25 11:12:34 +0100

    Add TotalFinder

---
+++ recipes/totalfinder.rb
@@ -0,0 +1,7 @@
+dmg_package "TotalFinder" do
+  source "http://downloads.binaryage.com/TotalFinder-1.4.8.dmg"
+  action :install
+  type "pkg"
+  owner node['current_user']
+  package_id "com.binaryage.pkg.totalfinder.app"
+end
