90d9be6da91c4c24ff7373433853612b36a7ad6f
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-03-02 22:42:22 +0100

    Change volume-dir

--- recipes/wireshark.rb
+++ recipes/wireshark.rb
@@ -2,6 +2,7 @@ include_recipe "applications::xquartz"
 
 dmg_package "Wireshark 1.8.5 Intel 64" do
   source "http://wiresharkdownloads.riverbed.com/wireshark/osx/Wireshark%201.8.5%20Intel%2064.dmg"
+  volumes_dir "Wireshark"
   action :install
   type "pkg"
   package_id "org.wireshark.Wireshark.pkg"
