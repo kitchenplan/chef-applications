18cb7e8df37797d55a65da9b0b6c9d2c9f875e6b
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-03-03 16:07:09 +0100

    Fix dmg-name

--- recipes/lastpass.rb
+++ recipes/lastpass.rb
@@ -1,6 +1,6 @@
-dmg_package "LastPass for Mac OS X" do
+dmg_package "lpmacosx" do
   source "https://download.lastpass.com/lpmacosx.dmg"
-  volumes_dir "lpmacosx"
+  volumes_dir "LastPass for Mac OS X"
   action :install
   type "pkg"
 #  package_id "org.wireshark.Wireshark.pkg"
