9d196bf0474d7bf9966f8913b2c97f9938898065
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-03-03 16:46:48 +0100

    Adobe wants to do difficult, new approach to install the Flash plugin

--- recipes/flash.rb
+++ recipes/flash.rb
@@ -1,7 +1,23 @@
-dmg_package "Install Adobe Flash Player" do
-  source "http://fpdownload.macromedia.com/get/flashplayer/pdc/11.6.602.171/install_flash_player_osx.dmg"
-  volumes_dir "Flash Player"
-  action :install
-  type "pkg"
-  package_id "com.adobe.pkg.FlashPlayer"
+unless File.exists?("#{Chef::Config[:file_cache_path]}/install_flash_player_osx.dmg")
+    remote_file "#{Chef::Config[:file_cache_path]}/install_flash_player_osx.dmg" do
+        source "http://fpdownload.macromedia.com/get/flashplayer/pdc/11.6.602.171/install_flash_player_osx.dmg"
+        owner node['current_user']
+    end
+end
+
+execute "attach install_flash_player_osx.dmg" do
+    command "hdiutil attach '#{Chef::Config[:file_cache_path]}/install_flash_player_osx.dmg'"
+    user node['current_user']
+    not_if "pkgutil --packages | grep flash"
+end
+
+execute "install Flash" do
+    command "installer -pkg \"/Volumes/Flash Player/Install Adobe Flash Player.app/Contents/Resources/Adobe Flash Player.pkg\" -target / -lang en"
+    not_if "pkgutil --packages | grep flash"
+end
+
+execute "detach install_flash_player_osx.dmg" do
+    command "hdiutil detach \"/Volumes/Flash Player\""
+    user node['current_user']
+    only_if "mount | grep -q 'Flash Player'"
 end
