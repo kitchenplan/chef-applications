5a8ef78680ac1ea641ee93bbb5549c34e24cea26
Roderik van der Veer <roderik@vanderveer.be>
2013-03-10 13:36:28 +0100

    Start multi platform support, cookbook will need renaming

--- recipes/bash4.rb
+++ recipes/bash4.rb
@@ -4,14 +4,18 @@ package "bash" do
   action :install
 end
 
-execute "set the root user shell to bash" do
-  command "dscl . -create /Users/root UserShell /usr/local/bin/bash"
-end
+if platform_family?('mac_os_x')
 
-execute "set the root user shell to bash" do
-  command "dscl . -create /Users/#{node['current_user']} UserShell /usr/local/bin/bash"
-end
+    execute "set the root user shell to bash" do
+      command "dscl . -create /Users/root UserShell /usr/local/bin/bash"
+    end
 
-link "/root" do
-    to "/var/root"
+    execute "set the root user shell to bash" do
+      command "dscl . -create /Users/#{node['current_user']} UserShell /usr/local/bin/bash"
+    end
+
+    link "/root" do
+        to "/var/root"
+    end
+
 end
