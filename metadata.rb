16e86e80775b1815e49b7ebf367504f46f91a021
Roderik van der Veer <roderik@vanderveer.be>
2013-03-10 14:26:09 +0100

    Fix metadata

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,12 @@ name             'homebrew'
 maintainer       'Roderik van der Veer'
 maintainer_email 'roderik@vanderveer.be'
 license          'Apache 2.0'
-description      'Installs and configures homebrew and homebrew apps'
+description      'Installs and configures the package manager and facilitates installation of apps through this manager. Homebrew and apt.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
-supports         "mac_os_x"
 
-depends apt
+%w{ mac_os_x ubuntu debian }.each do |os|
+  supports os
+end
+
+depends "apt"
