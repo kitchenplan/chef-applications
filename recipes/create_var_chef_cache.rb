4999551ceded5891a6144da91b7118d1d025737a
Roderik van der Veer <roderik@vanderveer.be>
2013-03-10 10:09:49 +0100

    Updated to support ubuntu

--- recipes/create_var_chef_cache.rb
+++ recipes/create_var_chef_cache.rb
@@ -1,6 +1,9 @@
 directory Chef::Config[:file_cache_path] do
   owner "root"
-  group "admin"
+  group value_for_platform(
+                            "mac_os_x" => { "default" => "admin" },
+                            "default" => "root"
+                          )
   mode 0777
   action :create
   recursive true
