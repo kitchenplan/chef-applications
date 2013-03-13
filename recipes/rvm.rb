f57687c4ca0630766b0ed68bde178b5d0267b338
Roderik van der Veer <roderik@vanderveer.be>
2013-03-13 09:11:32 +0100

    Install rvm dependencies

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -2,4 +2,10 @@ if platform?("mac_os_x")
     include_recipe applications::gcc42
 end
 
+%w[ openssl libxml2 libxslt libksba sqlite ].each do |pkg|
+    package pkg do
+        action [:install, :upgrade]
+    end
+end
+
 include_recipe rvm::system
