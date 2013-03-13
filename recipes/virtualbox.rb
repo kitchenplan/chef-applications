6eebb0700e3a47cc6c7e67411b0ff1690ae86ab4
Roderik van der Veer <roderik@vanderveer.be>
2013-03-13 09:24:02 +0100

    Upgrade virtualbox

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -1,5 +1,5 @@
 dmg_package "VirtualBox" do
-  source "http://download.virtualbox.org/virtualbox/4.2.6/VirtualBox-4.2.6-82870-OSX.dmg"
+  source "http://download.virtualbox.org/virtualbox/4.2.8/VirtualBox-4.2.8-83876-OSX.dmg"
   action :install
   owner node['current_user']
   type "pkg"
