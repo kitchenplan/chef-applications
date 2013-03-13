a71eaa1ec1622845a13c262143ba75a1e64d51fd
Roderik van der Veer <roderik@vanderveer.be>
2013-03-13 09:07:12 +0100

    Stray quotes removal

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -14,7 +14,7 @@ template "/etc/apache2/other/kdeploy.conf" do
 end
 
 %w{/etc/apache2/conf/projects.d /etc/apache2/logs /opt/nowebsite}.each do |pkg|
-  directory "pkg" do
+  directory pkg do
       owner "root"
       group value_for_platform(
                             "mac_os_x" => { "default" => "admin" },
