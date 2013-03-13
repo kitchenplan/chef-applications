1100810b7a8b5399b58e835a4c01012387bd10da
Roderik van der Veer <roderik@vanderveer.be>
2013-03-13 10:36:09 +0100

    Add OSX java

--- recipes/java.rb
+++ recipes/java.rb
@@ -3,6 +3,16 @@ dmg_package "Java 7 Update 15" do
   action :install
   type "pkg"
   package_id "com.oracle.jre"
+  only_if { platform_family?("mac_os_x") }
+end
+
+dmg_package "JavaForOSX" do
+  source "http://support.apple.com/downloads/DL1572/en_US/"
+  volumes_dir "Java for OS X 2013-002"
+  action :install
+  type "pkg"
+  package_id "com.apple.pkg.JavaForMacOSX107"
+  only_if { platform_family?("mac_os_x") }
 end
 
 #dmg_package "JDK 7 Update 13" do
