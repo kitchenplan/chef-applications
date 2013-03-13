76b536c9fa3bb19b58de1acda858c1e3265bcac0
Roderik van der Veer <roderik@vanderveer.be>
2013-03-13 09:17:10 +0100

    Fix foodcritic error

--- recipes/git.rb
+++ recipes/git.rb
@@ -1,15 +1,15 @@
 include_recipe "applications::default"
 include_recipe "applications::bash-completion"
 
-if platform?("ubuntu")
-    apt_repository "kdeploy" do
-      uri "http://ppa.launchpad.net/pdoes/ppa/ubuntu"
-      distribution node['lsb']['codename']
-      components ["main"]
-      keyserver "keyserver.ubuntu.com"
-      key "4CBEDD5A"
-      deb_src true
-    end
+
+apt_repository "kdeploy" do
+  uri "http://ppa.launchpad.net/pdoes/ppa/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "4CBEDD5A"
+  deb_src true
+  only_if platform?("ubuntu")
 end
 
 package "git" do
