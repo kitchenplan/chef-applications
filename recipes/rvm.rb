5628d7fa973ae9129bd3e3119630bffa6dcf663d
Roderik van der Veer <roderik@vanderveer.be>
2013-03-13 10:48:44 +0100

    More rvm fixes

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,5 +1,7 @@
+include_recipe "applications::default"
+
 if platform?("mac_os_x")
-    include_recipe applications::gcc42
+    include_recipe "applications::gcc42"
 end
 
 %w[ openssl libxml2 libxslt libksba sqlite ].each do |pkg|
