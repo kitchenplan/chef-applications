e33daa55a764e3fb67a123d6d9a1cc30432856f3
stephaneb <stephane.bakelants@kunstmaan.be>
2013-03-13 13:10:08 +0100

    changed recipes for debian family

--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,8 +1,11 @@
 if platform?("mac_os_x")
     include_recipe applications::gcc42
+	packages = %w[ openssl libxml2 libxslt libksba sqlite  ]
+elsif platform_family?("debian")
+    packages = %w[ openssl libxml2 libxslt1.1 libksba8 sqlite ]
 end
 
-%w[ openssl libxml2 libxslt libksba sqlite ].each do |pkg|
+packages.each do |pkg|
     package pkg do
         action [:install, :upgrade]
     end
