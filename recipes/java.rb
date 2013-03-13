29d01853d98b89ab18cd677b7eddeb7dd7b046fe
Roderik van der Veer <roderik@vanderveer.be>
2013-03-13 09:06:58 +0100

    Don't install the JDK, it's very slow

--- recipes/java.rb
+++ recipes/java.rb
@@ -5,9 +5,9 @@ dmg_package "Java 7 Update 15" do
   package_id "com.oracle.jre"
 end
 
-dmg_package "JDK 7 Update 13" do
-  source "https://s3.amazonaws.com/boxen-downloads/java/jdk-7u13.dmg"
-  action :install
-  type "pkg"
-  package_id "com.oracle.jdk7u13"
-end
+#dmg_package "JDK 7 Update 13" do
+#  source "https://s3.amazonaws.com/boxen-downloads/java/jdk-7u13.dmg"
+#  action :install
+#  type "pkg"
+#  package_id "com.oracle.jdk7u13"
+#end
