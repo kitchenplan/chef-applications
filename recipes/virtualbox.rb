fce0ee8cd2124a1817e6b548281a6086d70b3ab7
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-03-16 12:57:19 +0100

    Update virtualbox.rb

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -1,6 +1,6 @@
 if platform?("mac_os_x")
     dmg_package "VirtualBox" do
-        source "http://download.virtualbox.org/virtualbox/4.2.8/VirtualBox-4.2.8-83876-OSX.dmg"
+        source "http://download.virtualbox.org/virtualbox/4.2.10/VirtualBox-4.2.10-84104-OSX.dmg"
 		action :install
 		owner node['current_user']
 		type "pkg"
