acc5126ebca863465de2cc69975af5856432d0f6
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-03-16 08:07:50 +0100

    Upgrade to Vagrant 1.1 and add providers

--- recipes/vagrant.rb
+++ recipes/vagrant.rb
@@ -2,7 +2,7 @@ include_recipe "applications::virtualbox"
 
 if platform?("mac_os_x")
     dmg_package "Vagrant" do
-        source "http://files.vagrantup.com/packages/476b19a9e5f499b5d0b9d4aba5c0b16ebe434311/Vagrant.dmg"
+        source "http://files.vagrantup.com/packages/194948999371e9aee391d13845a0bdeb27e51ac0/Vagrant.dmg"
         action :install
         type "pkg"
         owner node['current_user']
@@ -13,3 +13,4 @@ elsif platform_family?("debian")
         action [:install, :upgrade]
     end
 end
+
