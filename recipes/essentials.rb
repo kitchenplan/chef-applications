b90e186681436d5f3293742bb7a9f36bc0886172
stephaneb <stephane.bakelants@kunstmaan.be>
2013-03-19 13:00:33 +0100

    postgres install + some required packages in essentials

--- recipes/essentials.rb
+++ recipes/essentials.rb
@@ -1,7 +1,13 @@
-package "build-essential" do
-    action [:install, :upgrade]
+#python-setuptools is needed for easy_install
+#python-dev is needed for the header files of python
+packages = %w[ build-essential python-setuptools python-dev]
+
+packages.each do |pkg|
+    package pkg do
+        action [:install, :upgrade]
+    end
 end
-#You can't place linux-headers-`uname -r` in a list, if you do it throws an error
+#You can't place linux-headers-`uname -r` in the list, if you do it throws an error
 package "linux-headers-`uname -r`" do
     action [:install, :upgrade]
 end
--- recipes/psycopg2.rb
+++ recipes/psycopg2.rb
@@ -1,9 +1,3 @@
 include_recipe "applications::postgresql"
 
-if platform?('mac_os_x')
-    easy_install_package "psycopg2"
-elsif platform_family?('debian')
-    package "python-psycopg2" do
-        action [:install, :upgrade]
-    end
-end
+easy_install_package "psycopg2"
