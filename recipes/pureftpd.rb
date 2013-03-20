2615f77ac56c517b4bbcc71d8a0183deebc1fd5a
stephaneb <stephane.bakelants@kunstmaan.be>
2013-03-20 15:35:30 +0100

    service restart pure-ftpd

--- recipes/pureftpd.rb
+++ recipes/pureftpd.rb
@@ -3,10 +3,15 @@ if platform_family?('debian')
         action [:install, :upgrade]
     end
     
+    service "pure-ftpd" do
+        supports [:restart]
+    end
+    
     execute "create MinUID" do
         cwd "/etc/pure-ftpd/conf"
         user "root"
         command 'echo "0" > MinUID'
         not_if { File.exists?("/etc/pure-ftpd/conf/MinUID") }
+        notifies :restart, "service[pure-ftpd]"
     end
 end
