cba341a934d3e324b2553e7680325ccbda7e17fd
stephaneb <stephane.bakelants@kunstmaan.be>
2013-03-25 14:58:22 +0100

    dropbox ubuntu install

--- recipes/dropbox.rb
+++ recipes/dropbox.rb
@@ -1,6 +1,14 @@
-dmg_package "Dropbox" do
-  volumes_dir "Dropbox Installer"
-  source "https://www.dropbox.com/download?plat=mac"
-  action :install
-  owner node['current_user']
+if platform?("mac_os_x")
+    dmg_package "Dropbox" do
+        volumes_dir "Dropbox Installer"
+        source "https://www.dropbox.com/download?plat=mac"
+        action :install
+        owner node['current_user']
+    end
+elsif platform_family?("debian")
+    include_recipe "apt"
+
+    package "nautilus-dropbox" do
+        action [:install, :upgrade]
+    end
 end
