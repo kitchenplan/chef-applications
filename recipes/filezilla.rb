de8f65e9920f7c0d36fb413f73f8507eeedb60ef
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-04-05 13:06:39 +0200

    filezilla dl link fix

--- recipes/filezilla.rb
+++ recipes/filezilla.rb
@@ -1,6 +1,6 @@
 if platform?('mac_os_x')
     remote_file "#{Chef::Config['file_cache_path']}/FileZilla_3.6.0.2_i686-apple-darwin9.app.tar.bz2" do
-        source "http://voxel.dl.sourceforge.net/project/filezilla/FileZilla_Client/3.6.0.2/FileZilla_3.6.0.2_i686-apple-darwin9.app.tar.bz2"
+        source "http://surfnet.dl.sourceforge.net/project/filezilla/FileZilla_Client/3.6.0.2/FileZilla_3.6.0.2_i686-apple-darwin9.app.tar.bz2"
     end
 
     execute "tar -xzf #{Chef::Config['file_cache_path']}/FileZilla_3.6.0.2_i686-apple-darwin9.app.tar.bz2" do
