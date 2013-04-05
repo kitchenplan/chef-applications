fd155b6ffc73158550b704e4902cea268285d1aa
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-04-05 09:46:33 +0200

    Adium

---
+++ recipes/adium.rb
@@ -0,0 +1,4 @@
+dmg_package 'Adium' do
+    source 'http://downloads.sourceforge.net/project/adium/Adium_1.5.6.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fadium%2Ffiles%2F%3Fsource%3Dnavbar&ts=1365147779&use_mirror=netcologne'
+    action :install
+end
