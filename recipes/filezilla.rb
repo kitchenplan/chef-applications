db60ac9dc0611d8f15cc5fac019e3fa9ebee0157
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-04-05 13:18:14 +0200

    fix for bz2

--- recipes/filezilla.rb
+++ recipes/filezilla.rb
@@ -3,7 +3,7 @@ if platform?('mac_os_x')
         source "http://surfnet.dl.sourceforge.net/project/filezilla/FileZilla_Client/3.6.0.2/FileZilla_3.6.0.2_i686-apple-darwin9.app.tar.bz2"
     end
 
-    execute "tar -xzf #{Chef::Config['file_cache_path']}/FileZilla_3.6.0.2_i686-apple-darwin9.app.tar.bz2" do
+    execute "tar -xjf #{Chef::Config['file_cache_path']}/FileZilla_3.6.0.2_i686-apple-darwin9.app.tar.bz2" do
         cwd "/Applications"
     end
 end
