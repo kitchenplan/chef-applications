d18d92ffd66a490dc25f7e6a46ee226f26ef808e
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-04-05 12:36:44 +0200

    adium volume name

--- recipes/adium.rb
+++ recipes/adium.rb
@@ -2,6 +2,6 @@ if platform?('mac_os_x')
     dmg_package 'Adium' do
         source "http://netcologne.dl.sourceforge.net/project/adium/Adium_1.5.6.dmg"
         action :install
-        volumes_dir "Adium_1.5.6"
+        volumes_dir "Adium 1.5.6"
     end
 end
