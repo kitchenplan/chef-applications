004058e87793e84e198ee9d14b675c099beadcd5
Roderik van der Veer <roderik@vanderveer.be>
2013-04-05 21:03:26 +0200

    Fix foodcritic errors

--- recipes/alfred.rb
+++ recipes/alfred.rb
@@ -1,7 +1,4 @@
-unless File.exists?("/Applications/Alfred 2.app")
-
-    applications_package "Alfred" do
-        source  "http://cachefly.alfredapp.com/Alfred_2.0.2_178.zip"
-    end
-
+applications_package "Alfred" do
+    source "http://cachefly.alfredapp.com/Alfred_2.0.2_178.zip"
+    not_if File.exists?("/Applications/Alfred 2.app")
 end
--- recipes/handbrake.rb
+++ recipes/handbrake.rb
@@ -1,7 +1,6 @@
-if platform?('mac_os_x')
-    dmg_package "HandBrake" do
-        source "http://ignum.dl.sourceforge.net/project/handbrake/0.9.8/HandBrake-0.9.8-MacOSX.6_GUI_x86_64.dmg"
-        action :install
-        volumes_dir "HandBrake-0.9.8-MacOSX.6_GUI_x86_64"
-    end
+dmg_package "HandBrake" do
+    source "http://ignum.dl.sourceforge.net/project/handbrake/0.9.8/HandBrake-0.9.8-MacOSX.6_GUI_x86_64.dmg"
+    action :install
+    volumes_dir "HandBrake-0.9.8-MacOSX.6_GUI_x86_64"
+    only_if platform?('mac_os_x')
 end
