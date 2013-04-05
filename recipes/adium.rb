efb848af360ccb590191dee6b07691d8785acb5c
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-04-05 12:31:03 +0200

    adium quote fix

--- recipes/adium.rb
+++ recipes/adium.rb
@@ -1,6 +1,6 @@
 if platform?('mac_os_x')
     dmg_package 'Adium' do
-        source 'http://netcologne.dl.sourceforge.net/project/adium/Adium_1.5.6.dmg"
+        source "http://netcologne.dl.sourceforge.net/project/adium/Adium_1.5.6.dmg"
         action :install
         volumes_dir "Adium_1.5.6"
     end
