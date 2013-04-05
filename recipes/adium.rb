b4e53345463fd08d9042c8e9de32fa4ad3d9b61f
Wim Vandersmissen <wim.vandersmissen@kunstmaan.be>
2013-04-05 12:29:39 +0200

    Adium volume fix

--- recipes/adium.rb
+++ recipes/adium.rb
@@ -1,6 +1,7 @@
 if platform?('mac_os_x')
     dmg_package 'Adium' do
-        source 'http://downloads.sourceforge.net/project/adium/Adium_1.5.6.dmg?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fadium%2Ffiles%2F%3Fsource%3Dnavbar&ts=1365147779&use_mirror=netcologne'
+        source 'http://netcologne.dl.sourceforge.net/project/adium/Adium_1.5.6.dmg"
         action :install
+        volumes_dir "Adium_1.5.6"
     end
 end
