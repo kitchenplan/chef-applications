81af557089d005433f09e34729af11f7d46c40a9
Stéphane Bakelants <stephane.bakelants@kunstmaan.be>
2013-04-10 17:06:09 +0200

    added nfs-kernel-server

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -7,7 +7,8 @@ if platform?("mac_os_x")
 		package_id "org.virtualbox.pkg.virtualbox"
     end
 elsif platform_family?("debian")
-    %w[ virtualbox virtualbox-guest-additions ].each do |pkg|
+    #nfs-kernel-server is needed for the shared folders
+    %w[ virtualbox virtualbox-guest-additions nfs-kernel-server ].each do |pkg|
         package pkg do
             action [:install, :upgrade]
         end
