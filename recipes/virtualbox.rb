9534bcbbb7a6e8b5b1719b019e823a4575cb6cbc
Stéphane Bakelants <stephane.bakelants@kunstmaan.be>
2013-04-16 16:29:30 +0200

    installs more up-to-date version of vagrant/virtualbox

--- recipes/virtualbox.rb
+++ recipes/virtualbox.rb
@@ -7,7 +7,19 @@ if platform?("mac_os_x")
 		package_id "org.virtualbox.pkg.virtualbox"
     end
 elsif platform_family?("debian")
-    package "virtualbox" do
-        action [:install, :upgrade]
+    apt_repository "virtualbox" do
+        uri "http://download.virtualbox.org/virtualbox/debian"
+        distribution node["lsb"]["codename"]
+        components ["contrib"]
+        keyserver "keys.gnupg.net"
+	key "54422A4B98AB5139"
+        action :add
+        notifies :run, "execute[apt-get update]", :immediately
+    end
+
+    %w[ virtualbox-4.2 ].each do |pkg|
+        package pkg do
+            action [:install, :upgrade]
+        end
     end
 end
