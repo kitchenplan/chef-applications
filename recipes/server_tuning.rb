295e1b5080cd058686c2f6f13c96830296ceb9d7
Roderik van der Veer <roderik@vanderveer.be>
2013-06-08 16:09:54 +0200

    Rename config

--- recipes/server_tuning.rb
+++ recipes/server_tuning.rb
@@ -1,15 +1,15 @@
 template "/etc/sysctl.d/50-tuning.conf" do
-    source "sysctl.d.50-tuning.conf.erb"
+    source "sysctltuning.erb"
     owner "root"
     group "root"
     mode "0644"
 end
-    
+
 execute "configure kernel parameters" do
     user "root"
     command "sysctl -p"
 end
-   
+
 template "/etc/security/limits.conf" do
     source "security.limits.conf.erb"
     owner "root"
