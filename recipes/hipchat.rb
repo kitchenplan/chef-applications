8ded590ad4306f2dfba931eb9808c8f35d1e4c01
Thomas Meeus <thomas.meeus@kunstmaan.be>
2013-06-12 10:34:40 +0200

    Install on OSX only

--- recipes/hipchat.rb
+++ recipes/hipchat.rb
@@ -1,3 +1,5 @@
-applications_package "HipChat" do
-  source  "http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.34-01uennzomg01wmg.zip"
+if platform?('mac_os_x')
+    applications_package "HipChat" do
+        source  "http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.34-01uennzomg01wmg.zip"
+    end
 end
