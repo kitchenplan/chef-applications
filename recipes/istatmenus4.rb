9c407a178e5517958bb50cd39bf1d3018c4f717f
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:24:34 +0200

    Upgrade to istatmenus 4.06

--- recipes/istatmenus4.rb
+++ recipes/istatmenus4.rb
@@ -1,5 +1,5 @@
 applications_package "iStat Menus" do
-    source  "http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus4.04.zip"
+    source  "http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus4.06.zip"
 end
 
 
