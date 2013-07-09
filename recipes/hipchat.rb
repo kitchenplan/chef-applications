69426244937ed11f3dd1f31f31dcfd6751ff3a0a
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:23:49 +0200

    Upgrade to Hipchat 2.0

--- recipes/hipchat.rb
+++ recipes/hipchat.rb
@@ -1,4 +1,4 @@
 applications_package "HipChat" do
-    source  "http://downloads.hipchat.com.s3.amazonaws.com/mac-beta/HipChat-0.34-01uennzomg01wmg.zip"
+    source  "http://downloads.hipchat.com.s3.amazonaws.com/osx/HipChat-2.0.zip"
     only_if { platform?('mac_os_x') }
 end
