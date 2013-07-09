3fa4e33a29a8ba44c11a4f864d4bdf8c2450dfc6
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:29:48 +0200

    Update to 1.0.2

--- recipes/sequelpro.rb
+++ recipes/sequelpro.rb
@@ -1,6 +1,6 @@
 dmg_package "Sequel Pro" do
   volumes_dir "Sequel Pro 1.0.1"
   owner node['current_user']
-  source "http://sequel-pro.googlecode.com/files/sequel-pro-1.0.1.dmg"
+  source "https://sequel-pro.googlecode.com/files/sequel-pro-1.0.2.dmg"
   action :install
 end
