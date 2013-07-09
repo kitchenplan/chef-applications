545cb5fe8f3b8d838be5ee25b2c12d77f880a8c6
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:28:55 +0200

    Update to RubyMine 5.4.3

--- recipes/rubymine.rb
+++ recipes/rubymine.rb
@@ -1,5 +1,5 @@
 dmg_package "RubyMine" do
-  source "http://download.jetbrains.com/ruby/RubyMine-5.0.2.dmg"
+  source "http://download.jetbrains.com/ruby/RubyMine-5.4.3.dmg"
   action :install
   owner node['current_user']
 end
