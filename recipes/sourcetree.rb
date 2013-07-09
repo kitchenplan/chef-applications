d82bf11c84217bb228e883f362244354c3603409
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:31:11 +0200

    Upgrade to SourceTree 1.6.2.2

--- recipes/sourcetree.rb
+++ recipes/sourcetree.rb
@@ -1,5 +1,5 @@
 dmg_package "sourcetree" do
-  source  "http://downloads.atlassian.com/software/sourcetree/SourceTree_1.5.7.1.dmg"
+  source  "http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.2.2.dmg"
   owner node['current_user']
   action :install
 end
