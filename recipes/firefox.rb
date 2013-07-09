e2102878fb72993e04d184d6b3bf21077706f726
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:22:29 +0200

    Upgrade to firefox 22

--- recipes/firefox.rb
+++ recipes/firefox.rb
@@ -1,6 +1,6 @@
 if platform?('mac_os_x')
   dmg_package "Firefox" do
-    source "https://download.mozilla.org/?product=firefox-19.0&os=osx&lang=en-US"
+    source "https://download.mozilla.org/?product=firefox-22.0&os=osx&lang=en-US"
     action :install
     owner node['current_user']
   end
