37c6fc745296a14b588a8869e8c186dd1bbe6bb3
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:19:57 +0200

    Update to Alfred 2.0.5 202

--- recipes/alfred.rb
+++ recipes/alfred.rb
@@ -1,4 +1,4 @@
 applications_package "Alfred" do
-    source "http://cachefly.alfredapp.com/Alfred_2.0.2_178.zip"
+    source "http://cachefly.alfredapp.com/Alfred_2.0.5_202.zip"
     not_if { File.exists?("/Applications/Alfred 2.app") }
 end
