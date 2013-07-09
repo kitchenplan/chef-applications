bee45ae105b5aefa44eb47ac04839935712ebb29
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-09 12:18:30 +0200

    Update to 1Password 3.8.21

--- recipes/1password.rb
+++ recipes/1password.rb
@@ -1,3 +1,3 @@
 applications_package "1Password" do
-  source  "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-3.8.20.zip"
+  source  "https://d13itkw33a7sus.cloudfront.net/dist/1P/mac/1Password-3.8.21.zip"
 end
