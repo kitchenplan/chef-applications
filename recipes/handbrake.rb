97959e67d6996a14de40019f525e4890afd22ce9
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-10 10:35:31 +0200

    Update handbrake.rb

--- recipes/handbrake.rb
+++ recipes/handbrake.rb
@@ -1,6 +1,6 @@
 dmg_package "HandBrake" do
-    source "http://ignum.dl.sourceforge.net/project/handbrake/0.9.8/HandBrake-0.9.8-MacOSX.6_GUI_x86_64.dmg"
+    source "http://downloads.sourceforge.net/project/handbrake/0.9.9/HandBrake-0.9.9-MacOSX.6_GUI_x86_64.dmg"
     action :install
-    volumes_dir "HandBrake-0.9.8-MacOSX.6_GUI_x86_64"
+    volumes_dir "HandBrake-0.9.9-MacOSX.6_GUI_x86_64"
     only_if { platform?('mac_os_x') }
 end
