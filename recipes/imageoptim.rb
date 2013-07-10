23004e9f56a8558767742d5a5f300fac7ed846a8
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-07-10 10:42:05 +0200

    Update imageoptim.rb

--- recipes/imageoptim.rb
+++ recipes/imageoptim.rb
@@ -2,19 +2,17 @@ unless File.exists?("/Applications/Imageoptim.app")
 
   remote_file "#{Chef::Config[:file_cache_path]}/ImageOptim.tbz2" do
     source "http://imageoptim.com/ImageOptim.tbz2"
-    owner WS_USER
-    checksum "0b1941ed047cac5e03668e04b4da7e58af99afa26af55c54cd1d62b564b11445"
+    owner node['current_user']
   end
 
   execute "unzip ImageOptim" do
     command "tar -xf #{Chef::Config[:file_cache_path]}/ImageOptim.tbz2 -C #{Chef::Config[:file_cache_path]}/"
-    user WS_USER
+    user node['current_user']
   end
 
   execute "copy ImageOptim to /Applications" do
     command "mv #{Chef::Config[:file_cache_path]}/ImageOptim.app #{Regexp.escape("/Applications/ImageOptim.app")}"
-    user WS_USER
-    group "admin"
+    user node['current_user']
   end
 
   ruby_block "test to see if ImageOptim.app was installed" do
