a73f1cf4ec01f6f58b8dbce3689fb4ff695a6ebe
Roderik van der Veer <roderik@vanderveer.be>
2013-10-04 22:37:45 +0200

    Upgrade adobe acrobat reader

--- recipes/adobe_reader.rb
+++ recipes/adobe_reader.rb
@@ -1,5 +1,5 @@
 dmg_package "Adobe Reader XI Installer" do
-  source "http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.00/en_US/AdbeRdr11000_en_US.dmg"
+  source "http://ardownload.adobe.com/pub/adobe/reader/mac/11.x/11.0.04/en_US/AdbeRdr11004_en_US.dmg"
   action :install
   volumes_dir "AdbeRdr11000_en_US"
   type "pkg"
