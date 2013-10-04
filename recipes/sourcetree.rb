58873998b0d47db924a8efdf7d1bbb20309b0ffc
Roderik van der Veer <roderik@vanderveer.be>
2013-10-04 23:24:55 +0200

    Use sourcetree from cask

--- recipes/sourcetree.rb
+++ recipes/sourcetree.rb
@@ -1,5 +1,2 @@
-dmg_package "sourcetree" do
-  source  "http://downloads.atlassian.com/software/sourcetree/SourceTree_1.6.2.2.dmg"
-  owner node['current_user']
-  action :install
-end
+include_recipe "applications::homebrewcask"
+applications_cask "sourcetree"
