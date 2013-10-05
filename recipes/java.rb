d26c94b75f2e1197b2311610e896a097dc434207
Roderik van der Veer <roderik@vanderveer.be>
2013-10-05 11:42:46 +0200

    Revert "Use java from kono_workstation instead"
    This reverts commit 988df76f137123ad21fed7140c32e35b10bd00a7.

---
+++ recipes/java.rb
@@ -0,0 +1,28 @@
+if platform?('mac_os_x')
+  dmg_package "Java 7 Update 15" do
+    source "http://javadl.sun.com/webapps/download/AutoDL?BundleId=74775"
+    action :install
+    type "pkg"
+    package_id "com.oracle.jre"
+  end
+
+# Does no longer
+#  dmg_package "JavaForOSX" do
+#    source "http://support.apple.com/downloads/DL1572/en_US/"
+#    volumes_dir "Java for OS X 2013-004"
+#    action :install
+#    type "pkg"
+#    package_id "com.apple.pkg.JavaForMacOSX107"
+#  end
+elsif platform_family?('debian')
+  package "openjdk-6-jdk" do
+    action [:install, :upgrade]
+  end
+end
+
+#dmg_package "JDK 7 Update 13" do
+#  source "https://s3.amazonaws.com/boxen-downloads/java/jdk-7u13.dmg"
+#  action :install
+#  type "pkg"
+#  package_id "com.oracle.jdk7u13"
+#end
