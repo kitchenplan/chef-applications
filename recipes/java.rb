82ef4f0e95da4d3482899c1fa7adedc889195bee
Roderik van der Veer <roderik@vanderveer.be>
2013-10-05 12:56:45 +0200

    Use the JDK

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,9 +1,9 @@
 if platform?('mac_os_x')
-  dmg_package "Java 7 Update 40" do
+  dmg_package "JDK 7 Update 40" do
     source "https://s3-eu-west-1.amazonaws.com/kitchenplan/jdk-7u40-macosx-x64.dmg"
     action :install
     type "pkg"
-    package_id "com.oracle.jre"
+    package_id "com.oracle.jdk7u40"
   end
 elsif platform_family?('debian')
   package "openjdk-6-jdk" do
