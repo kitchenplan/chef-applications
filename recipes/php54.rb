dcba0e2a7f091db659076b8696fefe42f1742a69
Roderik van der Veer <roderik@vanderveer.be>
2013-10-06 10:23:52 +0200

    Try brewed openssl

--- recipes/php54.rb
+++ recipes/php54.rb
@@ -6,15 +6,15 @@ if platform?('mac_os_x')
     applications_tap "homebrew/dupes"
 
     package "php54" do |variable|
-        options "--with-mysql --with-pgsql"
+        options "--with-mysql --with-pgsql --brewed-openssl"
     end
-    
+
     %w[ php54-apc php54-memcached php54-inclued php54-http php54-xdebug php54-intl php54-yaml php54-imagick php54-solr php54-twig php54-mcrypt].each do |pkg|
         package pkg do
             action [:install, :upgrade]
         end
     end
-    
+
     template "/usr/local/etc/php/5.4/conf.d/99-kunstmaan.ini" do
         source "php90kunstmaan.erb"
         owner node['current_user']
@@ -28,19 +28,19 @@ if platform?('mac_os_x')
     end
 elsif platform_family?('debian')
     packages = %w[ php5-mysqlnd php5-mcrypt php-apc php5-imagick php5-cli php5-gd php5-memcached php5-curl php5-intl php5-dev php5-sqlite php-pear libmagick++-dev ]
-    
+
     packages.each do |pkg|
         package pkg do
             action [:install, :upgrade]
         end
     end
-    
+
     template "/etc/php5/conf.d/99-kunstmaan.ini" do
         source "php90kunstmaan.erb"
         owner "root"
         mode "0755"
     end
-    
+
     template "/etc/php5/conf.d/50-apc.ini" do
         source "php50apc.erb"
         owner "root"
