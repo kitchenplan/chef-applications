b52bac66899bf6853b15b7450706b3c8891bdc12
Fernando Paredes <nano@fdp.io>
2013-10-13 17:44:07 -0700

    Update VMWare Fusion to follow brew-cask
    VMWare Fusion was recently added to homebrew-cask.

--- recipes/vmware_fusion.rb
+++ recipes/vmware_fusion.rb
@@ -1,6 +1,2 @@
-dmg_package "VMWare Fusion" do
-  source "http://download2us.softpedia.com/dl/a5bdb8f0e2faf4dcea62bf272213d56c/513376a7/400024122/mac/System-Utilities/VMware-Fusion-5.0.2-900491-light.dmg"
-  action :install
-  type "app"
-  package_id "com.vmware.tools.application"
-end
+include_recipe "applications::homebrewcask"
+applications_cask "vmware-fusion"
