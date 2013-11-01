5ccd34790250c3309a6964b9509d43a219fe9c3a
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2013-11-01 20:46:54 +0100

    Update composer.rb

--- recipes/composer.rb
+++ recipes/composer.rb
@@ -1,4 +1,11 @@
-composer "/usr/local/bin" do
-  owner "root" # optional
-  action [:install, :update]
+if platform?("mac_os_x")
+  composer "/usr/local/bin" do
+    owner "root" # optional
+    action [:install, :update]
+  end
+elsif platform_family?("debian")
+  composer "/usr/bin" do
+    owner "root" # optional
+    action [:install, :update]
+  end
 end
