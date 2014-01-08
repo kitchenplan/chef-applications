d20c8de91618ea57ec6eeb1f15d3df1e08d45661
Roderik van der Veer <roderik@vanderveer.be>
2014-01-08 09:42:45 +0100

    Feature: Add debian platform for hhvm recipe, and add the composer alias via hhvm for improved performance

--- recipes/hhvm.rb
+++ recipes/hhvm.rb
@@ -1,4 +1,19 @@
-applications_tap "homebrew/dupes"
-applications_tap "homebrew/versions"
-applications_tap "mcuadros/homebrew-hhvm"
+include_recipe "applications::packagemanager"
+include_recipe "applications::composer"
+
+case node["platform_family"]
+    when 'mac_os_x'
+        applications_tap "homebrew/dupes"
+        applications_tap "homebrew/versions"
+        applications_tap "mcuadros/homebrew-hhvm"
+    when 'debian'
+        apt_repository 'hhvm' do
+          uri          'http://dl.hhvm.com/ubuntu'
+          distribution node['lsb']['codename']
+          components   ['main']
+          key          'http://dl.hhvm.com/conf/hhvm.gpg.key'
+        end
+end
+
 package "hhvm"
+dotfiles_bash_it_custom_plugin "hhvm/composerviahhvm.bash"
---
+++ templates/default/hhvm/composerviahhvm.bash
@@ -0,0 +1,1 @@
+alias composer="hhvm /usr/local/bin/composer"
