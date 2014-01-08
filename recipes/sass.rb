333fa07be2cb0ab20f8b5199daf0f1e0bf89bcac
Roderik van der Veer <roderik@vanderveer.be>
2014-01-08 14:46:57 +0100

    Improvement: Install sass using native chef commands

--- recipes/sass.rb
+++ recipes/sass.rb
@@ -1,12 +1,1 @@
-
-execute "install sass" do
-        command "sudo gem install sass --no-ri --no-rdoc"
-        not_if { File.exist?("/usr/bin/sass") }
-end
-    
-if platform?('mac_os_x')
-    execute "create sass symlink" do
-        command "sudo ln -s `sudo -u #{node['current_user']} gem env gemdir`/bin/sass /usr/bin/sass"
-        not_if { File.exist?("/usr/bin/sass") }
-    end
-end
+gem_package "sass"
