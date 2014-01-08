f7708e4f86e161c3999fb581ab6281cfa6485bfb
Roderik van der Veer <roderik@vanderveer.be>
2014-01-08 09:41:29 +0100

    Feature: after setting up apt, also make sure all packages are updated

--- recipes/packagemanager.rb
+++ recipes/packagemanager.rb
@@ -22,5 +22,11 @@ case node["platform_family"]
             command "sudo sed 's@us.archive.ubuntu.com@be.archive.ubuntu.com@' -i /etc/apt/sources.list"
             action :run
         end
+
         include_recipe "apt::default"
+
+        execute 'apt-get-upgrade' do
+          command 'apt-get -y upgrade'
+        end
+
     end
