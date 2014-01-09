21600591a6d348e64e98a33c99194b18ebde542a
Roderik van der Veer <roderik@vanderveer.be>
2014-01-09 11:00:46 +0100

    Bugfix: use the Homebrew cookbook

--- recipes/packagemanager.rb
+++ recipes/packagemanager.rb
@@ -4,19 +4,7 @@ case node["platform_family"]
           owner node['current_user']
           recursive true
         end
-        homebrew_go = "#{Chef::Config[:file_cache_path]}/homebrew_go"
-        remote_file homebrew_go do
-            source "https://raw.github.com/mxcl/homebrew/go/install"
-            mode 00755
-        end
-        execute homebrew_go do
-            user node['current_user']
-            not_if { File.exist? '/usr/local/bin/brew' }
-        end
-        execute "update homebrew from github" do
-            user node['current_user']
-            command "/usr/local/bin/brew update || true"
-        end
+        include_recipe "homebrew::default"
     when 'debian'
         execute "change to a BE mirror" do
             command "sudo sed 's@us.archive.ubuntu.com@be.archive.ubuntu.com@' -i /etc/apt/sources.list"
@@ -28,5 +16,4 @@ case node["platform_family"]
         execute 'apt-get-upgrade' do
           command 'apt-get -y upgrade'
         end
-
-    end
+end
