fa6087cfa6deb6587db10fb55a58644bbbb93a0e
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-01-13 13:26:13 +0100

    Update skylab.rb

--- recipes/skylab.rb
+++ recipes/skylab.rb
@@ -1,4 +1,5 @@
 include_recipe "applications::php55"
+include_recipe "applications::rsync"
 
 execute "install skyab" do
     command "curl -sS https://raw.github.com/Kunstmaan/skylab/master/installer | php && mv skylab.phar /usr/local/bin/skylab"
