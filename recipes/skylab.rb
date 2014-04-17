1f4ea93a3ef7bec116ec9364a8ea50f98add8d82
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-04-17 21:42:10 +0200

    Add skylab postgres user

--- recipes/skylab.rb
+++ recipes/skylab.rb
@@ -45,3 +45,33 @@ template "#{otherlocation}/skylab.conf" do
         :user => node['current_user']
     )
 end
+
+include_recipe "applications::postgresql"
+
+postgresuser = value_for_platform(
+    ["ubuntu"] => { "default" => "postgres"},
+    "default" => node['current_user']
+)
+
+dblocation = value_for_platform(
+    ["ubuntu"] => { "default" => "/var/lib/postgresql"},
+    "default" => "/usr/local/var/postgres"
+)
+
+execute "create the postgresql Skylab superuser" do
+   command "psql -d template1 -c 'create user skylab with SUPERUSER;' && touch #{dblocation}/.create-postgresql-skylab-user-success"
+    user postgresuser
+    not_if do ::File.exists?("#{dblocation}/.create-postgresql-skylab-user-success") end
+end
+
+execute "create the postgresql Skylab superuser password" do
+    command "psql  -d template1 -c \"alter user skylab with password '#{node['skylab']['postgresql']['password']}';\" && touch #{dblocation}/.set-postgresql-skylab-password-success"
+    user postgresuser
+    not_if do ::File.exists?("#{dblocation}/.set-postgresql-skylab-password-success") end
+end
+
+execute "grant the postgresql Skylab superuser" do
+    command "psql -d template1 -c 'GRANT SELECT ON pg_shadow TO skylab;' && touch #{dblocation}/.grant-postgresql-skylab-user-success"
+    user postgresuser
+    not_if do ::File.exists?("#{dblocation}/.grant-postgresql-skylab-user-success") end
+end
