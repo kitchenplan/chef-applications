2ac7bfdda5837684fe95a49f0886fa508500c56b
mAAdhaTTah <jamesorodig@gmail.com>
2014-08-21 22:14:44 -0400

    Update php54 with 55 changes

--- recipes/php54.rb
+++ recipes/php54.rb
@@ -2,21 +2,21 @@ include_recipe "homebrewalt::default"
 include_recipe "applications::postgresql"
 include_recipe "applications::mysql"
 
-homebrewalt_tap "josegonzalez/php"
 homebrewalt_tap "homebrew/dupes"
+homebrewalt_tap "homebrew/homebrew-php"
 
 package "php54" do |variable|
     options "--with-mysql --with-pgsql --with-imap"
 end
 
-%w[ php54-apc php54-inclued php54-http php54-xdebug php54-intl php54-yaml php54-imagick php54-solr php54-mongo php54-memcached php54-twig php54-mcrypt].each do |pkg|
+%w[ php54-apc php54-inclued php54-http php54-xdebug php54-yaml php54-imagick php54-solr php54-mongo php54-memcached php54-twig php54-mcrypt].each do |pkg|
     package pkg do
         action [:install, :upgrade]
     end
 end
 
-template "/usr/local/etc/php/5.4/conf.d/99-kunstmaan.ini" do
-    source "php90kunstmaan.erb"
+template "/usr/local/etc/php/5.4/conf.d/99-extras.ini" do
+    source "phpini.erb"
     owner node['current_user']
     mode "0644"
 end
