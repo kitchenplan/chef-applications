5facd7e8b761a1ce61295b7eb76a8d6fd124b4ee
mAAdhaTTah <jamesorodig@gmail.com>
2014-08-21 22:10:29 -0400

    Fix bash to use homebrew version

--- recipes/bash.rb
+++ recipes/bash.rb
@@ -1,7 +1,14 @@
+include_recipe "homebrewalt::default"
+
 case node["platform_family"]
     when 'mac_os_x'
+        homebrewalt_tap "homebrew/dupes"
+        package "bash" do
+          action [:install, :upgrade]
+        end
         execute "set the root user shell to bash" do
-          command "dscl . -create /Users/root UserShell /bin/bash"
+          command "sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'"
+          command "sudo chsh -s /usr/local/bin/bash #{node['current_user']}"
         end
 
         link "/root" do
