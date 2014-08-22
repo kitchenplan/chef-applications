dceebfc1634805c66f77c6df9f061157e4d7d155
mAAdhaTTah <jamesorodig@gmail.com>
2014-08-21 21:50:57 -0400

    Remove php55-intl
    ```
    ○ → brew install php55-intl
    Error: No available formula for php55-intl
    ```

--- recipes/php55.rb
+++ recipes/php55.rb
@@ -9,7 +9,7 @@ package "php55" do |variable|
     options "--with-mysql --with-pgsql --with-apache"
 end
 
-%w[ php55-apcu php55-http php55-xdebug php55-intl php55-yaml php55-imagick php55-twig php55-mcrypt].each do |pkg|
+%w[ php55-apcu php55-http php55-xdebug php55-yaml php55-imagick php55-twig php55-mcrypt].each do |pkg|
     package pkg do
         action [:install, :upgrade]
     end
