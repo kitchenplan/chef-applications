283974fac21fa1992cfe9e94c165c5dfe4df8b0c
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-08-28 21:46:04 +0200

    Add a PHP 5.6 recipe

---
+++ recipes/php56.rb
@@ -0,0 +1,28 @@
+include_recipe "homebrewalt::default"
+include_recipe "applications::postgresql"
+include_recipe "applications::mysql"
+
+homebrewalt_tap "homebrew/dupes"
+homebrewalt_tap "homebrew/homebrew-php"
+
+package "php56" do |variable|
+    options "--with-mysql --with-pgsql --with-apache"
+end
+
+%w[ php56-apcu php56-http php56-xdebug php56-intl php56-yaml php56-imagick php56-twig php56-mcrypt].each do |pkg|
+    package pkg do
+        action [:install, :upgrade]
+    end
+end
+
+template "/usr/local/etc/php/5.6/conf.d/99-kunstmaan.ini" do
+    source "php90kunstmaan.erb"
+    owner node['current_user']
+    mode "0644"
+end
+
+template "/etc/apache2/other/php.conf" do
+  source "apache_php65.erb"
+  owner node['current_user']
+  mode "0755"
+end
