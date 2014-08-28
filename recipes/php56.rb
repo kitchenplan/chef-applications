3c5722961a4c215f25e0b6cb66bbd8700e69161e
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2014-08-28 22:55:24 +0200

    Update php56.rb

--- recipes/php56.rb
+++ recipes/php56.rb
@@ -1,12 +1,11 @@
 include_recipe "homebrewalt::default"
-include_recipe "applications::postgresql"
 include_recipe "applications::mysql"
 
 homebrewalt_tap "homebrew/dupes"
 homebrewalt_tap "homebrew/homebrew-php"
 
 package "php56" do |variable|
-    options "--with-mysql --with-pgsql --with-apache"
+    options "--with-mysql --with-apache"
 end
 
 %w[ php56-apcu php56-http php56-xdebug php56-mongo php56-yaml php56-imagick php56-twig php56-mcrypt].each do |pkg|
