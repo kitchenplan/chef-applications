54c08701f075b1f65206566ff72d5735d48078b0
Thomas Meeus <thomas.meeus@kunstmaan.be>
2014-09-10 10:20:15 +0200

    correct ssh_config location
    ssh_config on mac is located in /etc instead of /etc/ssh/ like on ubuntu

--- recipes/ssh_config.rb
+++ recipes/ssh_config.rb
@@ -1,26 +1,26 @@
 node["ssh"]["hosts"].each do |(host, port)|
     bash "set #{host}" do
         user "root"
-        cwd "/etc/ssh/"
+        cwd "/etc/"
         code <<-EOH
             echo >> ssh_config
             echo "Host #{host}" >> ssh_config
             echo "    Port #{port}" >> ssh_config
         EOH
-        not_if "grep -q #{host} /etc/ssh/ssh_config"
+        not_if "grep -q #{host} /etc/ssh_config"
     end
 end
 
 node["ssh"]["host_aliases"].each do |(host, hostname, port)|
     bash "set #{host}" do
         user "root"
-        cwd "/etc/ssh/"
+        cwd "/etc/"
         code <<-EOH
             echo >> ssh_config
             echo "Host #{host}" >> ssh_config
             echo "    HostName #{hostname}" >> ssh_config
             echo "    Port #{port}" >> ssh_config
         EOH
-        not_if "grep -q #{host} /etc/ssh/ssh_config"
+        not_if "grep -q #{host} /etc/ssh_config"
     end
 end
