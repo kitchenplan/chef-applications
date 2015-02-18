ea1d491c1228f0d6285bb81262884d946827da2d
Rick Guyer <ricoguyer@gmail.com>
2015-02-18 17:32:34 -0600

    Fix gem installation
    :warning: This fixes gem installation for `kitchenplan` by assuming
    commands and file locations. It is poorly implemented at the moment due
    to hard coding of the gem installation path.

--- recipes/gems.rb
+++ recipes/gems.rb
@@ -1,5 +1,8 @@
+#gem_install_dir = `gem env gemdir | tr -d "\n"`
+gem_install_dir = '/Library/Ruby/Gems/2.0.0'
 node['gems'].each do |gem|
   gem_package gem do
-    gem_binary '/usr/bin/ruby'
+    gem_binary("/usr/bin/gem")
+    options("--install-dir '#{gem_install_dir}' --bindir '/usr/bin'")
   end
 end
