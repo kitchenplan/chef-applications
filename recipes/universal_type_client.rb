6f59cd2593ef2b47c740b669afb655b8bff598a0
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-03-17 22:35:17 +0100

    Update universal_type_client.rb

--- recipes/universal_type_client.rb
+++ recipes/universal_type_client.rb
@@ -1,14 +1,17 @@
 remote_file "#{Chef::Config[:file_cache_path]}/UTC-4-2-3-M.zip" do
   source "http://bin.extensis.com/UTC-4-2-3-M.zip"
   owner node['current_user']
+  not_if { ::File.exists?("/Applications/Universal Type Client.app")}
 end
 
 execute "unzip UTC-4-2-3-M.zip" do
   command "unzip #{Chef::Config[:file_cache_path]}/UTC-4-2-3-M.zip -d #{Chef::Config[:file_cache_path]}/"
   user node['current_user']
+  not_if { ::File.exists?("/Applications/Universal Type Client.app")}
 end
 
 execute "install Universal Type Client 4.2.3.pkg" do
   command "installer -pkg '#{Chef::Config[:file_cache_path]}/Universal Type Client 4.2.3.pkg' -target /"
   action :run
+  not_if { ::File.exists?("/Applications/Universal Type Client.app")}
 end
