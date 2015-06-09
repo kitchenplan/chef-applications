c9d530c76d899f872eeb8d6b09fb5f54d69d8462
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-06-09 14:19:41 +0200

    Update homebrew-bash.rb

--- recipes/homebrew-bash.rb
+++ recipes/homebrew-bash.rb
@@ -8,11 +8,11 @@ end
 
 execute "add the homebrew bash to shells" do
   command "echo /usr/local/bin/bash >> /etc/shells && touch #{Chef::Config[:file_cache_path]}/.add-homebrew-bash-to-shells-success"
-  user root
+  user 'root'
   not_if do ::File.exists?("#{Chef::Config[:file_cache_path]}/.add-homebrew-bash-to-shells-success") end
 end
 
 execute "change shell for the current user" do
   command "chsh -s /usr/local/bin/bash #{node['current_user']}"
-  user root
+  user 'root'
 end
