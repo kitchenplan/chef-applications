9723f3a3edbf38be95ec1e02b1c897f808646379
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2015-06-09 14:17:50 +0200

    Update homebrew-bash.rb

--- recipes/homebrew-bash.rb
+++ recipes/homebrew-bash.rb
@@ -1,19 +1,18 @@
 include_recipe "homebrewalt::default"
 
-case node["platform_family"]
-    when 'mac_os_x'
-        homebrewalt_tap "homebrew/dupes"
-        package "bash" do
-          action [:install, :upgrade]
-        end
-        execute "set the root user shell to bash" do
-          command "sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'"
-          command "sudo chsh -s /usr/local/bin/bash #{node['current_user']}"
-        end
+homebrewalt_tap "homebrew/dupes"
 
-        link "/root" do
-            to "/var/root"
-        end
-    when 'debian'
-        Chef::Log.debug("This recipe is OSX only")
+package "bash" do
+  action [:install, :upgrade]
+end
+
+execute "add the homebrew bash to shells" do
+  command "echo /usr/local/bin/bash >> /etc/shells && touch #{Chef::Config[:file_cache_path]}/.add-homebrew-bash-to-shells-success"
+  user root
+  not_if do ::File.exists?("#{Chef::Config[:file_cache_path]}/.add-homebrew-bash-to-shells-success") end
+end
+
+execute "change shell for the current user" do
+  command "chsh -s /usr/local/bin/bash #{node['current_user']}"
+  user root
 end
